7022444b7e1efbe02c4c6966481780e1e15ef236
volodymyr-hrytsenko <volodyk753@gmail.com>
2020-09-02 14:43:26 +0300

    create and describe clases models

---
+++ Models/Manager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace car_dealership.Models{
+    public class Manager{
+        public int ID {get; set;}
+        public string Name {get; set;}
+        
+        public ICollection<SalesContract> SalesContract {get; set;}
+    }
+}
---
+++ Models/SalesContract.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace car_dealership.Models{
+    public class SalesContract{
+        public int ID {get; set;}
+        public DateTime Time {get; set;}
+        public int CarID {get; set;}
+        public int ManagerID {get; set;}
+        public int CustomerID {get; set;}
+
+        public Car Car  {get; set;}
+        public Manager Manager  {get; set;}
+        public Customer Customer  {get; set;}
+    }
+}
